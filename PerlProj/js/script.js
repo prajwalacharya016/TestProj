90d9c6b7fec66290bd86d22139903ba0ae738090
prajwal acharya <callmeprajwal@gmail.com>
2017-06-14 14:50:28 -0700

    Newer Version of Project

---
+++ PerlProj/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+$("#appointmentForm").hide();
+
+$("#newButton").click(function(){
+    $("#dashboard").hide();
+    $("#appointmentForm").show();
+});
+
+$("#searchButton").click(function(){
+		var searchData = $("#search").val();
+		getAppointments(searchData);		
+});
+
+$("#cancelButton").click(function(){
+	$("#appointmentForm").hide();
+	$("#dashboard").show();
+});
+
+
+// gets appointment based on the searchData
+function getAppointments(searchData){
+            
+			$.ajax({            
+            url: "getAppointments.pl",
+            data: { 
+                "search": searchData
+            },
+            type: "GET",
+            success: function(responseJson) {               
+                
+                // parse the result to JSON        
+                result =JSON.parse(responseJson);
+                //creates table
+                $("#tableArea").text("");
+                    var $table = $("<table class='table table-hover'>").appendTo($("#tableArea"));
+                        $table.append($("<thead>"));                        
+                        $table.append($("<th>").text("Date")); 
+                        $table.append($("<th>").text("Time"));
+                        $table.append($("<th>").text("Description"));
+                        $table.append($("</thead>"));
+                    $.each(result, function(index, appointment) {    
+                            var datetime = appointment.datetime.split(' ');
+                            $table.append($("<tbody>"));  
+                            $("<tr class ='active'>").appendTo($table)              
+                            .append($("<td>").text(datetime[0]))             
+                            .append($("<td>").text(datetime[1]))          
+                            .append($("<td>").text(appointment.description));
+                            $table.append($("</tbody>"));                    
+                    });
+                
+            }
+           
+
+		
+	});
+}
+	
+});
